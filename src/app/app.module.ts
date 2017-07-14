0ec2699e5a96c7024b068d4d02e64b14f785f8e9
David Wilson <davidtheadmiral@gmail.com>
2017-07-14 10:02:04 -0700

    add components for pages: home, about, members, member-details, bikes, admin

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,22 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { MembersComponent } from './members/members.component';
+import { MemberDetailsComponent } from './member-details/member-details.component';
+import { BikesComponent } from './bikes/bikes.component';
+import { AdminComponent } from './admin/admin.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    MembersComponent,
+    MemberDetailsComponent,
+    BikesComponent,
+    AdminComponent
   ],
   imports: [
     BrowserModule,
